85baf07a078b4e334c9efb3602844d4e210353d8
hoang.ausway <hoang.ausway@gmail.com>
2022-12-21 19:51:11 +1100

    Fix import.meta Parser Error

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,8 +1,15 @@
 import axios from 'axios'
-const BASE_URL = import.meta.env.VITE_APP_API_URL
-console.log('BASE_URL', BASE_URL)
-
-// const BASE_URL = 'https://cncserver-fayj4.ondigitalocean.app'
+const BASE_URL = import.meta.env.VITE_APP_API_URL // 'https://cncserver-fayj4.ondigitalocean.app'
+/* Note: To not get Parsr Error regarding 'import.meta...' 
+it should set 'parserOptions' in .eslintrc.json as below:
+{
+    ...,
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    }
+}
+*/
 
 export default axios.create({
     baseURL: BASE_URL,
