facf8154822e5ce26ea35b155929dacf48a0e011
hoang.ausway <hoang.ausway@gmail.com>
2022-12-24 21:08:48 +1100

    remove svelte query

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,7 @@
 <script>
-  import {QueryClient, QueryClientProvider} from '@sveltestack/svelte-query';
   import Header from './components/Header.svelte';
   import Router from './routing/Router.svelte';
-
-const queryClient = new QueryClient();
 </script>
 
-<QueryClientProvider client={queryClient}>
-  <Header />
-  <Router />
-</QueryClientProvider>
+<Header />
+<Router />
