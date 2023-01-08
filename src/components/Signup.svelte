770a154960b5fccc1fd42103d70d50f89a369634
hoang.ausway <hoang.ausway@gmail.com>
2023-01-08 16:42:21 +1100

    remove signup

---
+++ src/components/Signup.svelte
@@ -0,0 +1,25 @@
+<script>
+  import page from 'page';
+  import {useSignup} from '../states/useRemoteStates';
+
+  // credentials
+  const name = 'ABC 100';
+  const email = 'abc100@gmail.com';
+  const password = 'Hoang123';
+  const location = 'PR01';
+  const role = 'SELLER';
+
+  const signup = () => {
+    useSignup({email, password, location, name, role})
+      .then(() => page.redirect('/login'))
+      .catch(console.log);
+  };
+</script>
+
+<p>Name: {name}</p>
+<p>Email: {email}</p>
+<p>Password: {password}</p>
+<p>Location Id: {location}</p>
+<p>Role: {role}</p>
+
+<button on:click={signup}>Signup</button>
